a78e9e028b936641e507734e0d09a0c94e9d9577
arybary <l3fljl5ldydy@gmail.com>
2024-02-16 22:47:00 +0200

    add preprocessor & layout & component Header

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/stores';
+	import './Header.scss';
+</script>
+
+<header class="header">
+	<div class="header__nav">
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/contacts' ? 'page' : undefined}>
+				<a href="/contacts">About</a>
+			</li>
+		</ul>
+	</div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Header from '../components/Header/Header.svelte';
+	import './app.scss';
+</script>
+
+<div class="app">
+	<Header />
+	<main class="app__main">
+		<slot />
+	</main>
+	<footer class="app__footer">
+		<p>create by Medvid Rostislav</p>
+	</footer>
+</div>
